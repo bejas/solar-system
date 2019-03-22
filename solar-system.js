b2da7145a50717857e1d352ae5843c54f628c420
bejas <besarjasari@outlook.com>
2019-03-22 20:18:18 +0100

    added css file

--- solar-system.js
+++ solar-system.js
@@ -1,11 +1,15 @@
-window.onload = function () {
+window.onload = function () 
+{
+    // Constants
+    var speed = 0.0002;
+
     // Creating the scene
     var scene = new THREE.Scene();
 
     // Creating the camera
     var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
     camera.position.y = 0;
-    camera.position.z = 85;
+    camera.position.z = 95;
 
     // WebGL renderer
     var renderer = new THREE.WebGLRenderer();
@@ -17,17 +21,36 @@ window.onload = function () {
     orbitControl = new THREE.OrbitControls(camera, renderer.domElement);
     orbitControl.maxDistance = 200;
 
+    // Adding light
+    scene.add( new THREE.PointLight(0xffffff, 1) );
+    scene.add( new THREE.AmbientLight(0x404040) );
+
+    // Adding stars
+    stars = new THREE.Mesh(new THREE.SphereGeometry(300, 64, 64), new THREE.MeshBasicMaterial({ map: loader.load('textures/stars1.jpg'), side: THREE.BackSide }));
+    scene.add(stars);
+    stars.matrixAutoUpdate = false;
+
+    // Speed slider event
+    document.getElementById("speed").oninput = function (e) { speed = e.target.value / 10000; };
+
+    // Snackbar    
+    snackbar = document.getElementById("snackbar");
+    snackbar.className = "show";
+    setTimeout(function () { snackbar.className = snackbar.className.replace("show", ""); }, 3000);
+
     // Load objects
     var request = new XMLHttpRequest();
     request.open("GET", "./objects.json", false);
     request.send(null)
     var objects = JSON.parse(request.responseText);
 
-    for (var i in objects) {
+    for (var i in objects)
+    {
         o = objects[i];
         var geometry = new THREE.SphereGeometry(o.radius, 32, 32);
 
-        switch (o.material) {
+        switch (o.material) 
+        {
             case 'basic':
                 var material = new THREE.MeshBasicMaterial({ map: (new THREE.TextureLoader()).load('textures/' + o.texture) });
                 break;
@@ -43,22 +66,9 @@ window.onload = function () {
         else { objects[o.parent].mesh.add(o.mesh); }
     }
 
-
-    // Adding light
-    var sun_light = new THREE.PointLight(0xffffff, 1);
-    scene.add(sun_light);
-    var ambient_light = new THREE.AmbientLight(0x404040); // soft white ambient light
-    scene.add(ambient_light);
-
-    // Adding stars
-    stars = new THREE.Mesh(new THREE.SphereGeometry(300, 64, 64), new THREE.MeshBasicMaterial({ map: loader.load('textures/stars1.jpg'), side: THREE.BackSide }));
-    scene.add(stars);
-    stars.matrixAutoUpdate = false;
-
-    // Constants
-    var speed = 0.0002;
-
-    function animate() {
+    // Animate function
+    (function animate()
+    {
         var now = Date.now();
 
         for (var i in objects)
@@ -70,20 +80,10 @@ window.onload = function () {
             o.mesh.matrix = speedParentRot.multiply(tras.multiply(speedSelfRot));
         }
 
+        // Stars light movement
         stars.matrix = new THREE.Matrix4().makeRotationY(now * speed / 10);
 
         renderer.render(scene, camera);
         requestAnimationFrame(animate);
-    }
-
-    document.getElementById("speed").oninput = function (e) { speed = e.target.value / 10000; };
-
-    animate();
-    snackBarFun();
-}
-
-function snackBarFun() {
-    var x = document.getElementById("snackbar");
-    x.className = "show";
-    setTimeout(function () { x.className = x.className.replace("show", ""); }, 3000);
+    })();
 }
